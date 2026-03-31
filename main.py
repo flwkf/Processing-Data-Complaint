433c764f446667a614d98b23c65a3a1161bb6a88
Rizal Rahman Rizkika <94729829+flwkf@users.noreply.github.com>
2026-03-31 14:55:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ if uploaded_files:
     .tz_localize(None)
 )
 
-        merged_df['Bulan'] = (merged_df['Tgl Lapor'].dt.to_period('M').dt.to_timestamp())
+        merged_df['Bulan'] = (merged_df['Tgl Lapor'].dt.to_period('M').dt.to_timestamp()).dt.date
 
         merged_df['Tanggal'] = merged_df['Tgl Lapor'].dt.day
 
